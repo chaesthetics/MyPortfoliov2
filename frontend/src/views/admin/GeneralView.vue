ed594e439d1d9460f8d440a7f0f18ba9880716a9
Auriel James <aurieljames11@gmail.com>
2023-10-23 22:34:35 +0800

    issue/#30 SideBar Active Nav Fixed

--- frontend/src/views/admin/GeneralView.vue
+++ frontend/src/views/admin/GeneralView.vue
@@ -29,8 +29,8 @@ onMounted(()=>{
         <img class="h-[160px] w-[160px] rounded-full" src="https://jira.atlassian.com/secure/projectavatar?pid=18514&avatarId=106292">
         <div class="space-y-4">
             <div class="flex space-x-3 items-center">
-                <button class="py-2 px-6 bg-neutral-800 text-white rounded-md hover:bg-neutral-900 shadow">Edit profile</button>
-                <button class="py-2 px-6 bg-neutral-800 text-white rounded-md hover:bg-neutral-900 shadow">View Site</button>
+                <button class="py-2 px-6 bg-neutral-800 text-white rounded-md hover:bg-green-700 shadow">Edit profile</button>
+                <button class="py-2 px-6 bg-neutral-800 text-white rounded-md hover:bg-green-700 shadow">View Site</button>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings text-neutral-400"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
             </div>
             <div>
