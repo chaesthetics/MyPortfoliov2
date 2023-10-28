e6fc88a2c787475d4c59f65461038a544e4ed697
Auriel James <aurieljames11@gmail.com>
2023-10-29 03:30:49 +0800

    issue/#42 Backend Is Done in Testing

---
+++ backend/app/Http/Controllers/Api/ProjectsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;    
+use Illuminate\Support\Facades\Auth;
+
+class ProjectsController extends Controller
+{
+    public function getProjects()
+    {
+        return response()->json("Welcome to my projects");
+    }
+
+    public function createProject(Request $request)
+    {
+        $project = new Project();
+
+        try{
+            $customMessage = [
+                "title.required" => "Title field is required",
+                "description.required" => "Description field is required",
+                "img.required" => "Must have an image",
+                "language.required" => "Langauge field is required",
+                "role.required" => "Role field is required",
+                "link.required" => "Link is required",
+            ];
+            
+            $validatedData = $request->validate([
+                "title" => "required", 
+                "description" => "required",
+                "img" => "required",
+                "language" => "required",
+                "role" => "required",
+                "link" => "required",
+            ], $customMessage);
+
+            $project = Project::create($validatedData);
+
+            return response()->json([
+                "status" => true,
+                "message" => "Project succesfully create",
+            ]);
+        }
+        catch(\Throwable $th){
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage(),
+            ], 500);
+        }
+    }
+}
